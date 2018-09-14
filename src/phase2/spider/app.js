9ae6d80a2cda47e13c39793800ecff8131ed8549
代晓璐 <2428903784@qq.com>
2018-09-14 19:09:38 +0800

    爬虫1

--- src/phase2/spider/app.js
+++ src/phase2/spider/app.js
@@ -1,41 +1,23 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
+var request = require('request');
+const cheerio = require('cheerio');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/', function (req, res) {
+  var request = require('request');
+  request('https://www.jikexueyuan.com/', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      $ = cheerio.load(body);//$此刻拿到了整个body前端选择器
+      res.json({
+        'Classnum':$('.aside-cList li').length
+      })
+      console.log(body)
+      res.send('hi daisy')
+    } else {
+      console.log('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    }
+  });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(3000);
